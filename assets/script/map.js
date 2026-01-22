8425a5c37002cb9d171e17331def4d22ae36a395
Oxbian <oxbian@mailbox.org>
2026-01-22 14:52:12 +0100

    feat: adding icon for events

--- assets/script/map.js
+++ assets/script/map.js
@@ -7,6 +7,27 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//Icons
+var greenIcon = L.icon({
+    iconUrl: './assets/img/leaf-green.png',
+    shadowUrl: './assets/img/leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var pinIcon = L.icon({
+    iconUrl: './assets/img/pin.png',
+
+    iconSize:     [38, 95], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 function getUserLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setPosition, showError);
@@ -22,6 +43,7 @@ function setPosition(position) {
 
     // Centre la carte sur la position de l'utilisateur
     map.setView([userLat, userLng], 12);
+    L.marker([userLat, userLng], {icon: pinIcon}).addTo(map);
 }
 
 // Callback pour gérer les erreurs de permissions à la carte
@@ -45,7 +67,7 @@ function showError(error) {
 // Ajout des marqueurs pour chaque événement
 function addMarkers(events) {
     events.forEach(function(event) {
-        var marker = L.marker([event.latitude, event.longitude]).addTo(map);
+        var marker = L.marker([event.latitude, event.longitude], {icon: greenIcon}).addTo(map);
         marker.bindPopup(
             "<strong>" + event.ville + "</strong><br>" +
             "Déchets visés : " + event.dechets_vises_kg + " kg<br>" +
